9d40ffc297933f777b47a1f60ec22ded2ce6174e
Melisa7L <linf9801@gmail.com>
2025-10-12 23:37:35 -0300

    cambios2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-catalogo/',
+  base: '/Creaciones-Gabriela/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
