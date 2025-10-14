a7f526061f733a7c613ab8da3854ed66888ea235
Fátima Melisa Lino <117290413+Melisa7L@users.noreply.github.com>
2025-10-14 17:50:18 -0300

    rty

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '/Creaciones-Gabriela/',  
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
